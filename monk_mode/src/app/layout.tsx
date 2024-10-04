a8fd9039a6845549104669f7f48947b7888758b6
Ani <anirudhrahul@anjurahul.com>
2024-10-04 01:09:41 -0700

    add analytics

--- monk_mode/src/app/layout.tsx
+++ monk_mode/src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/react"
 import localFont from "next/font/local";
 import "./globals.css";
 
@@ -36,6 +37,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <Analytics />
       </body>
     </html>
   );
