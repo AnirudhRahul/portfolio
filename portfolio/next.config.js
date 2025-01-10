7d44441a576cba5c1edd78dc6328afddac95ab5b
Ani <anirudhrahul@anjurahul.com>
2025-01-10 15:24:26 -0500

    remove old portfolio page

--- portfolio/next.config.js
+++
@@ -1,8 +0,0 @@
-const withPreact = require('next-plugin-preact')
-
-module.exports = {
-  reactStrictMode: true,
-  images: {
-    domains: ['raw.githubusercontent.com'],
-  },
-}
--- portfolio/pages/bg.js
+++
@@ -1,7 +0,0 @@
-import Background from '../components/Background.js';
-
-export default function BackgroundPage(){
-  return <Background/>
-}
-
-BackgroundPage.displayName = "BackgroundPage"
